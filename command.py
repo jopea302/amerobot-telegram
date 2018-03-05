c290a1db8850d808b50ee5f9aae5ca8ef98c924c
jopea302 <36991020+jopea302@users.noreply.github.com>
2018-03-05 16:25:51 -0300

    Update command.py

--- command.py
+++ command.py
@@ -10,7 +10,7 @@ from telegram.ext.dispatcher import run_async
 from telegram.ext import Updater
 from html import escape
 
-updater = Updater(token='BOT_TOKEN')
+updater = Updater(token='550133015:AAHlnT6Ho_XqEBNzF59TeLqaqbAutgOAHgU')
 dispatcher = updater.dispatcher
 
 import logging
@@ -19,17 +19,17 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 def commands(bot, update):
 	user = update.message.from_user.username 
-	bot.send_message(chat_id=update.message.chat_id, text="Initiating commands /tip & /withdraw have a specfic format,\n use them like so:" + "\n \n Parameters: \n <user> = target user to tip \n <amount> = amount of reddcoin to utilise \n <address> = reddcoin address to withdraw to \n \n Tipping format: \n /tip <user> <amount> \n \n Withdrawing format: \n /withdraw <address> <amount>")
+	bot.send_message(chat_id=update.message.chat_id, text="Initiating commands /tip & /withdraw have a specfic format,\n use them like so:" + "\n \n Parameters: \n <user> = target user to tip \n <amount> = amount of amero to utilise \n <address> = amero address to withdraw to \n \n Tipping format: \n /tip <user> <amount> \n \n Withdrawing format: \n /withdraw <address> <amount>")
 
 def help(bot, update):
-	bot.send_message(chat_id=update.message.chat_id, text="The following commands are at your disposal: /hi , /commands , /deposit , /tip , /withdraw , /price , /marketcap or /balance")
+	bot.send_message(chat_id=update.message.chat_id, text="The following commands are at your disposal: /hi , /commands , /deposit , /tip , /withdraw , /donate  or /balance")
 
 def deposit(bot, update):
 	user = update.message.from_user.username
 	if user is None:
 		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
 	else:
-		address = "/usr/local/bin/reddcoind"
+		address = "/usr/local/bin/amero-cli"
 		result = subprocess.run([address,"getaccountaddress",user],stdout=subprocess.PIPE)
 		clean = (result.stdout.strip()).decode("utf-8")
 		bot.send_message(chat_id=update.message.chat_id, text="@{0} your depositing address is: {1}".format(user,clean))
@@ -42,13 +42,13 @@ def tip(bot,update):
 	if user is None:
 		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
 	else:
-		machine = "@Reddcoin_bot"
+		machine = "@AmeroTipBot"
 		if target == machine:
 			bot.send_message(chat_id=update.message.chat_id, text="HODL.")
 		elif "@" in target:
 			target = target[1:]
 			user = update.message.from_user.username 
-			core = "/usr/local/bin/reddcoind"
+			core = "/usr/local/bin/amero-cli"
 			result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
 			balance = float((result.stdout.strip()).decode("utf-8"))
 			amount = float(amount)
@@ -60,7 +60,7 @@ def tip(bot,update):
 				balance = str(balance)
 				amount = str(amount) 
 				tx = subprocess.run([core,"move",user,target,amount],stdout=subprocess.PIPE)
-				bot.send_message(chat_id=update.message.chat_id, text="@{0} tipped @{1} of {2} RDD".format(user, target, amount))
+				bot.send_message(chat_id=update.message.chat_id, text="@{0} tipped @{1} of {2} AMX".format(user, target, amount))
 		else: 
 			bot.send_message(chat_id=update.message.chat_id, text="Error that user is not applicable.")
 
@@ -77,30 +77,14 @@ def balance(bot,update):
 	if user is None:
 		bot.send_message(chat_id=update.message.chat_id, text="Please set a telegram username in your profile settings!")
 	else:
-		core = "/usr/local/bin/reddcoind"
+		core = "/usr/local/bin/amero-cli"
 		result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
 		clean = (result.stdout.strip()).decode("utf-8")
 		balance  = float(clean)
 		fiat_balance = balance * price
 		fiat_balance = str(round(fiat_balance,3))
 		balance =  str(round(balance,3))
-		bot.send_message(chat_id=update.message.chat_id, text="@{0} your current balance is: {1} RDD ≈  ${2}".format(user,balance,fiat_balance))
-
-def price(bot,update):
-	quote_page = requests.get('https://www.worldcoinindex.com/coin/reddcoin')
-	strainer = SoupStrainer('div', attrs={'class': 'row mob-coin-table'})
-	soup = BeautifulSoup(quote_page.content, 'html.parser', parse_only=strainer)
-	name_box = soup.find('div', attrs={'class':'col-md-6 col-xs-6 coinprice'})
-	name = name_box.text.replace("\n","")
-	price = re.sub(r'\n\s*\n', r'\n\n', name.strip(), flags=re.M)
-	fiat = soup.find('span', attrs={'class': ''})
-	kkz = fiat.text.replace("\n","")
-	percent = re.sub(r'\n\s*\n', r'\n\n', kkz.strip(), flags=re.M)
-	quote_page = requests.get('https://bittrex.com/api/v1.1/public/getticker?market=btc-rdd')
-	soup = BeautifulSoup(quote_page.content, 'html.parser').text
-	btc = soup[80:]
-	sats = btc[:-2]
-	bot.send_message(chat_id=update.message.chat_id, text="Reddcoin is valued at {0} Δ {1} ≈ {2}".format(price,percent,sats) + " ฿")
+		bot.send_message(chat_id=update.message.chat_id, text="@{0} your current balance is: {1} AMX".format(user,balance))
 
 def withdraw(bot,update):
 	user = update.message.from_user.username
@@ -112,7 +96,7 @@ def withdraw(bot,update):
 		address = ''.join(str(e) for e in address)
 		target = target.replace(target[:35], '')
 		amount = float(target)
-		core = "/usr/local/bin/reddcoind"
+		core = "/usr/local/bin/amero-cli"
 		result = subprocess.run([core,"getbalance",user],stdout=subprocess.PIPE)
 		clean = (result.stdout.strip()).decode("utf-8")
 		balance = float(clean)
@@ -121,7 +105,7 @@ def withdraw(bot,update):
 		else:
 			amount = str(amount)
 			tx = subprocess.run([core,"sendfrom",user,address,amount],stdout=subprocess.PIPE)
-			bot.send_message(chat_id=update.message.chat_id, text="@{0} has successfully withdrew to address: {1} of {2} RDD" .format(user,address,amount))
+			bot.send_message(chat_id=update.message.chat_id, text="@{0} has successfully withdrew to address: {1} of {2} AMX" .format(user,address,amount))
 
 def hi(bot,update):
 	user = update.message.from_user.username
@@ -130,14 +114,12 @@ def hi(bot,update):
 def moon(bot,update):
   bot.send_message(chat_id=update.message.chat_id, text="Moon mission inbound!")
 
-def marketcap(bot,update):
-	quote_page = requests.get('https://www.worldcoinindex.com/coin/reddcoin')
-	strainer = SoupStrainer('div', attrs={'class': 'row mob-coin-table'})
-	soup = BeautifulSoup(quote_page.content, 'html.parser', parse_only=strainer)
-	name_box = soup.find('div', attrs={'class':'col-md-6 col-xs-6 coin-marketcap'})
-	name = name_box.text.replace("\n","")
-	mc = re.sub(r'\n\s*\n', r'\n\n', name.strip(), flags=re.M)
-	bot.send_message(chat_id=update.message.chat_id, text="The current market cap of Reddcoin is valued at {0}".format(mc))
+def donate(bot,update):
+  bot.send_message(chat_id=update.message.chat_id, text="You can donate your Ameros here: AGVDMTRmN2FQAcDThPEkZaPfq6yntj9Ri6")
+
+def hayunacosaclara(bot,update):
+    bot.send_message(chat_id=update.message.chat_id, text="Hay que tener bitcoin sí o sí")
+    
 
 from telegram.ext import CommandHandler
 
@@ -150,17 +132,17 @@ dispatcher.add_handler(moon_handler)
 hi_handler = CommandHandler('hi', hi)
 dispatcher.add_handler(hi_handler)
 
+donate_handler = CommandHandler('donate', donate)
+dispatcher.add_handler(donate_handler)
+
 withdraw_handler = CommandHandler('withdraw', withdraw)
 dispatcher.add_handler(withdraw_handler)
 
-marketcap_handler = CommandHandler('marketcap', marketcap)
-dispatcher.add_handler(marketcap_handler)
+hayunacosaclara_handler = CommandHandler('hayunacosaclara', hayunacosaclara)
+dispatcher.add_handler(hayunacosaclara_handler)
 
 deposit_handler = CommandHandler('deposit', deposit)
 dispatcher.add_handler(deposit_handler)
-
-price_handler = CommandHandler('price', price)
-dispatcher.add_handler(price_handler)
 
 tip_handler = CommandHandler('tip', tip)
 dispatcher.add_handler(tip_handler)
@@ -172,4 +154,3 @@ help_handler = CommandHandler('help', help)
 dispatcher.add_handler(help_handler)
 
 updater.start_polling()
-
